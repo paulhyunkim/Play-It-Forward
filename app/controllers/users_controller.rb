a293beb766b6f78e2ef30d9286ea18aa4917ad94
Aaron Tsang <aarontsang@Aarons-MacBook-Pro.local>
2014-06-03 11:38:19 -0700

    attempting to resolve styling issues regarding require tree

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
-
-
 class UsersController < ApplicationController
+	before_action :authenticate_user, :except => [:new, :create]
 
 	def index
 		client = RdioApi.new(:consumer_key => "px6jnus8n8e94qg7u9fbhpsc", :consumer_secret => "qJzVAj7PxJ")
@@ -12,7 +11,12 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@uri = "spotify:track:1ZA8L9IOkRhmmz4nPXXx7h"
+		client = RdioApi.new(:consumer_key => "px6jnus8n8e94qg7u9fbhpsc", :consumer_secret => "qJzVAj7PxJ")
+	  if params[:search]
+	    @songs = client.search(:query => params[:search], :types => "track", count: 10).results
+	  else
+	    @songs = []
+	  end
 	end
 
 
@@ -20,25 +24,16 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
-	
 
-	def show
-		client = RdioApi.new(:consumer_key => "px6jnus8n8e94qg7u9fbhpsc", :consumer_secret => "qJzVAj7PxJ")
-	  	if params[:search]
-	    	@songs = client.search(:query => params[:search], :types => "track", count: 10).results
-	  	else
-	    	@songs = []
-	  	end
-	end
 
 	def create
 		@user = User.new(user_params)
 		if @user.save
 			# session[:remember_token] = @user.id
 			# @current_user = @user
-			session[:remember_token] = @user.id.to_s
+			# session[:remember_token] = @user.id.to_s
 			flash.now[:success] = "You have succesfully signed up!"
-			redirect_to user_path(current_user)
+			redirect_to users_path
 		else
 			render 'new'
 		end
