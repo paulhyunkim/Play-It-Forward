7c4211a81cdd80c8fe7e137cd74ed9bf2ca0f45f
Aaron Tsang <aarontsang@Aarons-MacBook-Pro.local>
2014-06-04 16:13:21 -0700

    removed jquery-min, fixed landing page animations

---
+++ app/assets/javascripts/b.js
@@ -0,0 +1,89 @@
+
+$(document).ready(function(){
+
+	$('#introbox #play').hide().fadeIn(2000);
+	$('#introbox h2').hide().delay(700).fadeIn(2000);
+// ----------------------------------------------------
+
+}); //closes document.ready
+$(function(){ $(document).foundation(); });
+
+
+
+//Nav bar fade. Disappear by 2nd div.
+$(function(){
+    var fadeBegin = 500, 
+    fadeFinish = 690, 
+    fadingElement = $('.nav');
+
+$(window).on('scroll', function(){
+    var offset = $(document).scrollTop(), opacity = 0; 
+    console.log(offset);
+    if( offset <= fadeBegin ){
+        opacity = 1; 
+    } else if( offset <= fadeFinish ){
+        opacity = 1 - offset / fadeFinish;
+
+    }
+    fadingElement.css('opacity',opacity);
+});
+    
+    });
+
+
+//Scroll to top div
+  $(document).ready(function(){
+    $('a[href="#introbox"]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target
+        || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+          var targetOffset = $target.offset().top;
+          $('html,body')
+          .animate({scrollTop: targetOffset}, 1000);
+         return false;
+        }
+      }
+    });
+  });
+
+
+
+//Scroll to second div
+  $(document).ready(function(){
+    $('a[href="#whoarewe"]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target
+        || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+          var targetOffset = $target.offset().top;
+          $('html,body')
+          .animate({scrollTop: targetOffset}, 1000);
+         return false;
+        }
+      }
+    });
+  });
+
+//Scroll to bottom div
+  $(document).ready(function(){
+    $('a[href="#signup"]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target
+        || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+          var targetOffset = $target.offset().top;
+          $('html,body')
+          .animate({scrollTop: targetOffset}, 1000);
+         return false;
+        }
+      }
+    });
+  });
+  
