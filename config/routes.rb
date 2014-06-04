fcb77ca688d96bf4e555ea96785fad24b2a67e19
Aaron Tsang <aarontsang@Aarons-MacBook-Pro.local>
2014-06-03 21:26:10 -0700

    Implemented user index styling 15%

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ PlayItForward::Application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+ 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
