d05b17ff8179b18135cdf55ad57f1f65d0ac977c
Paul Kim <paulhyunkim@gmail.com>
2014-06-04 20:41:56 -0700

    removed geocoder gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,6 @@ class User < ActiveRecord::Base
 	has_many :playlists
 	has_many :songs
 
-	reverse_geocoded_by :latitude, :longitude
-	after_validation :reverse_geocode
-
 	def range
     %{ %s / %s AS range} % [distance_to_point, METERS_IN_A_MILE]
   end
